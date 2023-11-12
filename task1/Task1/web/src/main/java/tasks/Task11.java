b11aff90071ae94022c6b68642f688de1d09bec6
Nikita Poleev <poleev.nikita@mail.ru>
2023-11-12 17:27:19 +0300

    task 1 rebild

---
+++ task1/Task1/web/src/main/java/tasks/Task11.java
@@ -0,0 +1,65 @@
+package tasks;
+
+import java.io.*;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "task11", value = "/task11")
+public class Task11 extends HttpServlet {
+
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String  browserDetails  =  request.getHeader("User-Agent");
+        String  userAgent   =   browserDetails;
+        String  user  = userAgent.toLowerCase();
+        String browser = "";
+
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+
+
+        if (user.contains("msie"))
+        {
+            String substring=userAgent.substring(userAgent.indexOf("MSIE")).split(";")[0];
+            browser=substring.split(" ")[0].replace("MSIE", "IE")+"-"+substring.split(" ")[1];
+        } else if (user.contains("safari") && user.contains("version"))
+        {
+            browser=(userAgent.substring(userAgent.indexOf("Safari")).split(" ")[0]).split("/")[0]+"-"+(userAgent.substring(userAgent.indexOf("Version")).split(" ")[0]).split("/")[1];
+        } else if ( user.contains("opr") || user.contains("opera"))
+        {
+            if(user.contains("opera"))
+                browser=(userAgent.substring(userAgent.indexOf("Opera")).split(" ")[0]).split("/")[0]+"-"+(userAgent.substring(userAgent.indexOf("Version")).split(" ")[0]).split("/")[1];
+            else if(user.contains("opr"))
+                browser=((userAgent.substring(userAgent.indexOf("OPR")).split(" ")[0]).replace("/", "-")).replace("OPR", "Opera");
+        } else if (user.contains("chrome"))
+        {
+            browser=(userAgent.substring(userAgent.indexOf("Chrome")).split(" ")[0]).replace("/", "-");
+        } else if ((user.indexOf("mozilla/7.0") > -1) || (user.indexOf("netscape6") != -1)  || (user.indexOf("mozilla/4.7") != -1) || (user.indexOf("mozilla/4.78") != -1) || (user.indexOf("mozilla/4.08") != -1) || (user.indexOf("mozilla/3") != -1) )
+        {
+            //browser=(userAgent.substring(userAgent.indexOf("MSIE")).split(" ")[0]).replace("/", "-");
+            browser = "Netscape-?";
+
+        } else if (user.contains("firefox"))
+        {
+            browser=(userAgent.substring(userAgent.indexOf("Firefox")).split(" ")[0]).replace("/", "-");
+        } else if(user.contains("rv"))
+        {
+            browser="IE-" + user.substring(user.indexOf("rv") + 3, user.indexOf(")"));
+        } else
+        {
+            browser = "UnKnown, More-Info: "+userAgent;
+        }
+        out.println("<h2>" + "Privet polzovatel " + browser + "</h2>");
+        out.println("</html>");
+
+    }
+}
---
+++ task1/Task1/web/src/main/java/tasks/Task12.java
@@ -0,0 +1,50 @@
+package tasks;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import javax.imageio.ImageIO;
+
+@WebServlet(name = "task12", value = "/task12")
+public class Task12 extends HttpServlet {
+
+    private int hitCount;
+
+    public void init() {
+        // Reset hit counter.
+        hitCount = 0;
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        destroy();
+
+        response.setContentType("image/jpeg");
+
+        BufferedImage image =new BufferedImage(500,200 ,
+                BufferedImage.TYPE_INT_BGR);
+
+        String str = String.valueOf(hitCount++);
+
+        Graphics2D graphics = image.createGraphics();
+        graphics.setFont(new Font("Serif",Font.ITALIC,48));
+        graphics.setColor(Color.green);
+        graphics.drawString(str,100,100);
+
+        ServletOutputStream out = response.getOutputStream();
+        ImageIO.write(image, "jpeg",out);
+
+
+    }
+
+}
